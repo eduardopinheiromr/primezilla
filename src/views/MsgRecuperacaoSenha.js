6ede8b717e40daa16f3d062a08d08c5b16b7cf62
Victoria Marques <vicck.rj@gmail.com>
2020-07-24 15:01:36 -0300

    MsgRecuperacaoSenha.js adicionado

---
+++ src/views/MsgRecuperacaoSenha.js
@@ -0,0 +1,13 @@
+class MsgRecuperacaoSenha {
+    constructor() { throw new Error("Não é possível instanciar esta classe!")}
+
+    static montaLayout() {
+        return `
+            <div class="d-flex flex-column container justify-content-center mt-3">
+                <h2 class="text-center texto-verde">E-mail enviado com sucesso!</h2>
+                <a href="/index.html" class="mt-3 mb-5 texto-verde link-retorno">Voltar para o início</a>
+            </div>	
+        `;
+    }
+
+}

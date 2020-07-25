7203b047bbfb06871c77ef0844b4ada5b7af6ddd
Victoria Marques <vicck.rj@gmail.com>
2020-07-24 23:21:15 -0300

    ajustes na EnderecoController.js

--- src/controllers/EnderecoController.js
+++ src/controllers/EnderecoController.js
@@ -2,27 +2,51 @@ class EnderecoController {
     constructor() { throw new Error("Não é possível instanciar essa classe!") }
 
 
-    static geraEndereco() {
+    static geraEndereco(cep) {
+        
+        cep = cep.split("-").join("");
+
+        if(cep.length <= 7 || cep.length > 8) {
+            mensagemErro.classList.remove("invisivel");
+            campoCEP.value = "";
+            campoLogradouro.value = "";
+            campoCidade.value = "";
+            campoEstado.value = "";
+            setTimeout(mensagemDeErro,2000);
+        }
+
         let reqEndereco = new XMLHttpRequest;
 
-        reqEndereco.open("GET", `https://viacep.com.br/ws/${campoCEP.value}/json/`);
-
+        reqEndereco.open("GET", `https://viacep.com.br/ws/${cep}/json/`);
+     
         reqEndereco.addEventListener("load", () => {
-            
-            let reqJSON = JSON.parse(reqEndereco.responseText);
-        
-            let enderecoUsuario = {
-                logradouro: reqJSON["logradouro"],
-                bairro: reqJSON["bairro"],
-                cidade: reqJSON["localidade"],
-                estado: reqJSON["uf"]
-            }
+            if(reqEndereco.status == 200) {
+                let reqJSON = JSON.parse(reqEndereco.responseText);
+                if(reqJSON.erro) {
 
-            
-            console.log(enderecoUsuario.logradouro)
-            console.log(enderecoUsuario.bairro)
-            console.log(enderecoUsuario.cidade)
-            console.log(enderecoUsuario.estado)
+                    mensagemErro.classList.remove("invisivel");
+                    
+                    campoCEP.value = "";
+                    limpaFormulario();
+
+                } else {
+                   
+                    mensagemErro.classList.add("invisivel");
+
+                    limpaFormulario();
+
+                    let enderecoUsuario = {
+                        logradouro: reqJSON["logradouro"],
+                        bairro: reqJSON["bairro"],
+                        cidade: reqJSON["localidade"],
+                        estado: reqJSON["uf"]
+                    }
+    
+                    campoLogradouro.value = enderecoUsuario.logradouro;
+                    campoCidade.value = enderecoUsuario.cidade;
+                    campoEstado.value = enderecoUsuario.estado;
+                }
+            }
         });
 
        reqEndereco.send();
